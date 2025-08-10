d4512e62cab7fc8069f1825f929de6d5ffe79a31
Alexander Stauch <blacke4dawn@gmail.com>
2025-08-10 13:51:18 +0200

    minimum navbar in header

--- packages/frontend/src/siteHeader/siteHeader.tsx
+++ packages/frontend/src/siteHeader/siteHeader.tsx
@@ -1,6 +1,11 @@
+import HeaderNavbar from "./components/navbar/navbar";
+
 function SiteHeader() {
   return (
-    <h1>React App</h1>
+    <>
+      <h1>React App</h1>
+      <HeaderNavbar />
+    </>
   )
 }
 
