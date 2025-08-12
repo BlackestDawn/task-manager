c46d99c3359877e4e773afda6fd701934ad08cd5
Alexander Stauch <blacke4dawn@gmail.com>
2025-08-12 19:03:16 +0200

    dark mode switching

--- packages/frontend/components/footer/site.tsx
+++ packages/frontend/components/footer/site.tsx
@@ -1,5 +1,5 @@
 export default function SiteFooter() {
   return (
-    <p className="border-t-2 border-gray-200">Footer Content</p>
+    <p className="border-t-2 border-gray-900 dark:border-gray-200">Footer Content</p>
   )
 }
