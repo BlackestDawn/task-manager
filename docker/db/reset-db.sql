acc8a26b6f7c37eae16324b61a4a20912c8df483
Alexander Stauch <blacke4dawn@gmail.com>
2025-10-26 19:32:37 +0100

    updated PW in reset-db.sql

--- docker/db/reset-db.sql
+++ docker/db/reset-db.sql
@@ -5,4 +5,4 @@ DELETE FROM groups;
 DELETE FROM refresh_tokens;
 
 INSERT INTO users (login, name, password, access_level)
-VALUES ('admin', 'Administrator', '$2b$10$rd..08azGOvnYFpAR/Gb..ljhTyaPLRRG48tCIByDIfXQL8POHvF.', 'admin');
+VALUES ('admin', 'Administrator', '$2b$10$NH1Iuy.ABCvVI6.4rO406O2/.YIvCyF9JFXJMEJU6rHC7AHkFdgmy', 'admin');
